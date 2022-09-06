52a8b6383ccf2573d0f9a07efca6fd4a01641e20
Matt Bowen <mattbowen03@gmail.com>
2022-09-06 15:10:07 -0500

    removed unneeded functions

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import { useState } from "react";
 import { getReddit } from "./utils/getReddit";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [reddit, setReddit] = useState("Loading...");
-
-  const handleIncrement = () => {
-    console.log("going up!");
-    setCount((prev) => prev + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount((prev) => prev - 1);
-  };
+  const [reddit, setReddit] = useState("Click to access Reddit API...");
 
   const handleClick = async () => {
     const temp = await getReddit();
@@ -22,12 +12,6 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='counter-wrapper'>
-        <h1>Counter!</h1>
-        <input type='number' value={count} data-testid='poop'></input>
-        <button onClick={handleDecrement}>Decrement</button>
-        <button onClick={handleIncrement}>Increment</button>
-      </div>
       <div className='reddit-wrapper'>
         <h1>Reddit!</h1>
         <h2>{reddit}</h2>
--- src/utils/getReddit.js
+++ src/utils/getReddit.js
@@ -2,7 +2,6 @@ const getReddit = async () => {
   try {
     const result = await fetch("https://www.reddit.com/r/ooni/.json");
     const data = await result.json();
-
     return data.kind;
   } catch (e) {
     return "error!";
